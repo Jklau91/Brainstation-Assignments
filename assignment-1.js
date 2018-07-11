a08b05913ec5d8acc2a2bdd749bf76aee4415da2
Jklau91 <40874338+Jklau91@users.noreply.github.com>
2018-07-10 23:48:58 -0700

    Add files via upload

---
+++ assignment-1.js
@@ -0,0 +1,19 @@
+var numbers = [];
+
+for (var i=0; i < 10; i++){
+    numbers.push(i);
+
+console.log(number[1])
+console.log(number[9])
+
+}
+
+
+
+
+var car = {};
+
+
+car.colour = {black};
+
+
---
+++ assignment-2.js
@@ -0,0 +1,27 @@
+// Challenge 1
+var Hash = '#';
+var Start = '';
+for (var i = 0; i < 7; i++) {
+    Start = Start+Hash
+    console.log(Start)
+  }
+  
+  /*
+For this challenge, I used line 2 and 3 to state the variables that I am going to use. 
+Moving forward, I used a for loop to complete the triangle. I started the equation "Start=Start+Hash". 
+By doing so, the first line will be "#" and adding an additional one for next line. This process is 
+repeated 7 times.
+  */
+  
+  // Challenge 2
+  
+  function isEven(number) {
+      if(number%2==0) 
+      {0 = "the number is even";} else {0 = "the number is odd"}
+
+    /* For this challenge, I constructed a function that should tell if a number is odd or even. 
+    Once a number is inputted, the equation in the function will divde by 2 closest to zero. 
+    If there is a remainder, then the function will note "this number is odd", if there is no 
+    remainder, then the function will note "this number is even"*/
+  }
+  
